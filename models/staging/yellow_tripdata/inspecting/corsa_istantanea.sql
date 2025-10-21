14469df2e878e0efdc691d18d01b0dd246e37962
Cristian Porzio <c.porzio02@gmail.com>
2025-10-21 16:09:22 +0200

    refactor(yellow_tripdata): add inspection queries for trip costs and passenger counts

---
+++ models/staging/yellow_tripdata/inspecting/corsa_istantanea.sql
@@ -0,0 +1,13 @@
+SELECT
+    SUM(CASE WHEN NOT(DATEDIFF('minute', pickup, dropoff) BETWEEN 1 AND (24*60)) AND total_amount = 0 THEN 1 ELSE 0 END) as istantanei_e_gratis,
+    SUM(CASE WHEN NOT(DATEDIFF('minute', pickup, dropoff) BETWEEN 1 AND (24*60)) AND total_amount != 0 THEN 1 ELSE 0 END) as istantanei_e_non_gratis,
+    SUM(CASE WHEN NOT(DATEDIFF('minute', pickup, dropoff) BETWEEN 1 AND (24*60)) AND total_amount < 0 THEN 1 ELSE 0 END) as istantanei_e_negativi
+FROM {{ ref('stg_2_trip__cleaned') }}
+
+/*
+Previewing node 'corsa_istantanea':
+| istantanei_e_gratis | istantanei_e_non_... | istantanei_e_nega... |
+| ------------------- | -------------------- | -------------------- |
+|                 160 |                82140 |                 5037 |
+
+*/
--- models/staging/yellow_tripdata/inspecting/durata_corsa.sql
+++ models/staging/yellow_tripdata/inspecting/durata_corsa.sql
@@ -19,4 +19,25 @@ where not(DATEDIFF('minute', pickup, dropoff) BETWEEN 1 AND (24*60)) AND total_a
 
 -- vanno eliminati almeno gli spostamenti dove: start_location = end_location ed il loro totale è 0 
 
+
+    vendorid AS vendor_id,
+    tpep_pickup_datetime as pickup,
+    tpep_dropoff_datetime as dropoff,
+    ratecodeid AS rate_code_id,
+    store_and_fwd_flag,
+    pulocationid AS start_location_id,
+    dolocationid AS end_location_id,
+    passenger_count,
+    trip_distance,
+    fare_amount,
+    extra,
+    mta_tax,
+    tip_amount,
+    tolls_amount,
+    improvement_surcharge,
+    congestion_surcharge,
+    airport_fee,
+    payment_type,
+    total_amount
+
 */
