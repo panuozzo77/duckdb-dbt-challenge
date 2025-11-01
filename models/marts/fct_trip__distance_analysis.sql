2679418382a795b0780af5f100424f06ec064967
Cristian Porzio <c.porzio02@gmail.com>
2025-11-01 21:57:18 +0100

    fix(staging): add test to validate total_amount consistency

--- models/marts/fct_trip__distance_analysis.sql
+++ models/marts/fct_trip__distance_analysis.sql
@@ -27,4 +27,20 @@ Previewing node 'fct_trip__distance_analysis':
 | long              | 16.709.874,600… |     93,065… |      51,467… |      179551 |
 | medium            | 24.460.742,740… |     42,806… |      25,380… |      571426 |
 | short             | 32.164.149,000… |     19,175… |      10,335… |     1677368 |
+
+Dopo pulizia calcolo prezzo totale
+Previewing node 'fct_trip__distance_analysis':
+| distance_category |   total_revenue | avg_revenue | avg_duration | total_trips |  $/min |
+| ----------------- | --------------- | ----------- | ------------ | ----------- | ------ |
+| short             | 18.036.123,470… |     17,588… |       8,834… |     1025481 | 1,991… |
+| medium            | 14.496.962,140… |     27,986… |      17,987… |      518004 | 1,556… |
+| long              | 27.825.234,170… |     67,926… |      38,159… |      409641 | 1,780… |
+
+Senza pulizia sul calcolo totale
+Previewing node 'fct_trip__distance_analysis':
+| distance_category |   total_revenue | avg_revenue | avg_duration | total_trips |  $/min |
+| ----------------- | --------------- | ----------- | ------------ | ----------- | ------ |
+| short             | 22.798.477,430… |     17,469… |       8,786… |     1305104 | 1,988… |
+| medium            | 17.794.044,630… |     27,863… |      17,951… |      638616 | 1,552… |
+| long              | 32.645.497,130… |     67,737… |      37,657… |      481946 | 1,799… |
 */
---
+++ models/staging/yellow_tripdata/inspecting/sum_amount.sql
@@ -0,0 +1,10 @@
+
+
+
+
+select
+    *
+from "yellow_tripdata"."main"."stg_trip__cleaned"
+
+where not(total_amount >= fare_amount + extra + mta_tax + tip_amount + tolls_amount + improvement_surcharge + congestion_surcharge + airport_fee)
+
